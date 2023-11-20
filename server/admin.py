ac283713baa46300fca6e6b11d0648fb3c0784b4
YJ_ <Pumbaaxx@icloud.com>
2023-11-20 00:28:15 -0800

    admin data query

--- server/admin.py
+++ server/admin.py
@@ -11,8 +11,20 @@ def dashboard():
     print(current_user.admin)
     if current_user.admin:
         # get all users from the database
-        users_json = users.find({})
-        return render_template('admin.html', user=current_user, users=users_json)
+        users_data = users.find({})
+        users_num = users.count_documents({})
+        records_num = 0
+        bricks_num = 0
+        for user in users.find():
+            records_num += len(user.get("records", []))
+            bricks_num += len(user.get("bricks", {}))
+        return render_template('admin.html',
+                               user=current_user,
+                               users=users_data,
+                               users_num=users_num,
+                               records_num=records_num,
+                               bricks_num=bricks_num)
     else:
         print("not admin")
         return render_template('home.html', user=current_user)
+
